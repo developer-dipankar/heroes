b1a7e35fa43da7b0aff4012c1d77af9135db7c72
Tier5 LLC <github@tier5.us>
2018-02-09 17:20:58 +0530

    tutorial_1 #complete

---
+++ src/app/hero.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import {Hero} from '../Models/hero.model';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+
+import { MessageService } from './message.service';
+
+@Injectable()
+export class HeroService {
+
+  heroes = [
+      { id: 11, name: 'Mr. Nice' },
+      { id: 12, name: 'Narco' },
+      { id: 13, name: 'Bombasto' },
+      { id: 14, name: 'Celeritas' },
+      { id: 15, name: 'Magneta' },
+      { id: 16, name: 'RubberMan' },
+      { id: 17, name: 'Dynama' },
+      { id: 18, name: 'Dr IQ' },
+      { id: 19, name: 'Magma' },
+      { id: 20, name: 'Tornado' }
+    ];
+
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
+    return of(this.heroes);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // Todo: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(this.heroes.find(hero => hero.id === id));
+  }
+
+  // getHeroes(): Hero[] {
+  //   return this.heroes;
+  // }
+}
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../Models/hero.model';
+import { HeroService } from '../hero.service';
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm',
+  }
+
+  // heroes = [
+  //   { id: 11, name: 'Mr. Nice' },
+  //   { id: 12, name: 'Narco' },
+  //   { id: 13, name: 'Bombasto' },
+  //   { id: 14, name: 'Celeritas' },
+  //   { id: 15, name: 'Magneta' },
+  //   { id: 16, name: 'RubberMan' },
+  //   { id: 17, name: 'Dynama' },
+  //   { id: 18, name: 'Dr IQ' },
+  //   { id: 19, name: 'Magma' },
+  //   { id: 20, name: 'Tornado' }
+  // ];
+
+  heroes: Hero[];
+
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  // getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+
+}
